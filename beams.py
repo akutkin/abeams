9083ffd5f46d8ecc7c618067b2a8cc63cdeeeaef
AK <kutkin@gmail.com>
2020-10-28 09:34:39 +0000

    KMeans clustering to reduce the total samples; spectral index correction; no normalization etc...

--- beams.py
+++ beams.py
@@ -26,7 +26,6 @@ from astropy.wcs import WCS
 from radio_beam import Beam, Beams
 
 import shutil
-from atools.utils import fits_transfer_coordinates, fits_squeeze, fits_crop, fits_clip
 from reproject.mosaicking import reproject_and_coadd, find_optimal_celestial_wcs
 from reproject import reproject_interp
 from multiprocessing import Pool
@@ -48,23 +47,34 @@ import pickle as pkl
 
 import bdsf
 
+from sklearn.cluster import KMeans
+
 if not '/home/kutkin/apertif' in sys.path:
     sys.path.append('/home/kutkin/apertif')
 
+from atools.utils import fits_transfer_coordinates, fits_squeeze, fits_crop, fits_clip
 from atools.utils import fits_reconvolve_psf, get_tid_beam, fits_operation, fits_transpose
 from atools.catalogs import cross_match, load_nvss
 from dataqa.continuum.validation_tool import (radio_image, validation)
 
-
 nvss_psf = Beam(45*u.arcsec, 45*u.arcsec, 0*u.deg)
 
-def process(beam):
+
+
+def pybdsf_process(fitsfile):
+    pass
+
+
+def prepare_aptf_nvss_relation(beam):
+
+    nvss_cat = load_nvss()
+
     beam = str(beam).zfill(2)
     if not os.path.exists(f'/kutkin/beams/{beam}'):
         os.mkdir(f'/kutkin/beams/{beam}')
     files = glob.glob('/kutkin/acat/dr1/*_{}.fits'.format(beam))
     logging.info('%d files for beam %s', len(files), beam)
-    df2 = load_nvss()
+
     x = []
     y = []
     z = []
@@ -78,31 +88,34 @@ def process(beam):
         result = f'/kutkin/beams/{beam}/APERTIF_NVSS_relation.dat'
 
         # if os.path.exists(result):
-        #     logging.debug('Result file exists %s', result)
-        #     continue
+            # logging.debug('Result file exists %s', result)
+            # continue
         if os.path.exists(commoncat) and os.path.exists(hdrfile):
-            df = pd.read_csv(commoncat)
-            wcs = WCS(hdrfile).dropaxis(-1).dropaxis(-1)
+            continue
+            # df = pd.read_csv(commoncat)
+            # wcs = WCS(hdrfile).dropaxis(-1).dropaxis(-1)
         else:
             tmp = f'/kutkin/beams/{beam}/{tid}_{beam}_tmp.fits'
             copyfile(f, tmp)
-    # reconvolve with NVSS PSF:
+        # reconvolve with NVSS PSF:
             tmp=fits_reconvolve_psf(tmp, nvss_psf)
-    # source finding
+        # source finding
             img = bdsf.process_image(tmp)
             img.write_catalog(outfile=cat1, format='csv', clobber=True, catalog_type='srl')
-    # save header
+        # save header
             hdr = fits.getheader(tmp)
             wcs = WCS(hdr).dropaxis(-1).dropaxis(-1)
-            df = cross_match(cat1, df2)
+            df = cross_match(cat1, nvss_cat)
             df.to_csv(commoncat, index=False)
             hdr.tofile(hdrfile)
             os.remove(tmp)
+
         radec = [list(df.RA_1.values), list(df.DEC_1.values)]
         xi, yi = wcs.all_world2pix(radec[0], radec[1], 1)
         xi = [_-1537 for _ in xi]
         yi = [_-1537 for _ in yi]
         zi = list(df.Total_flux_1.values/df.Total_flux_2.values)
+        del df
         x += xi
         y += yi
         z += zi
@@ -157,7 +170,7 @@ def gppredict(gp, shape=750, size=100, normalize=True):
         newerr = err / (np.nanmax(arr) - np.nanmin(arr))
         return newarr, newerr, x1x2
     else:
-        return arr, err, x1x2
+        return arr+abs(gp.y_train_.min()), err, x1x2
 
 
 def tid2dt(tid):
@@ -178,7 +191,8 @@ def _data_within_dt(tid, beam, dt):
 
 
 def make_pbeam(tid, beam, maxdt=7, nnear=None, clip=0.05, shape=750, size=100,
-               gpfile=None, save_gp_to=None, out=None):
+               gpfile=None, save_gp_to=None, out=None,
+               normalize=False, spec_index_corr=True):
     """
     Create primary beam model (FITS file)
 
@@ -196,14 +210,21 @@ def make_pbeam(tid, beam, maxdt=7, nnear=None, clip=0.05, shape=750, size=100,
         the shape to predict GP on (full image is 1536, driftscan pbeams are 500... 750 should include 1st null level
     size : int, optionsl
         the size of the output array (NAXIS), driftscans imgaes have 40...
+    gpfile : use that for GP prediction if provided
+    save_gp_to : name of file to save the trained GP
+    out : name of the output GPbeam
+    normalize : scale the output beam to [0,1]
+    spec_index_corr : correct for the spectral index between APTF and NVSS
+
     Returns
     -------
-    None.
+    gp, data, err, out.
     """
 
     beam = str(beam).zfill(2)
-    tid = int(tid)
-    logging.info('Making pbeam for %s %s (date: %s)', tid, beam, tid2dt(tid))
+    if tid:
+        tid = int(tid)
+        logging.info('Making pbeam for %s %s (date: %s)', tid, beam, tid2dt(tid))
 
 # GP
     if gpfile is not None:
@@ -225,11 +246,14 @@ def make_pbeam(tid, beam, maxdt=7, nnear=None, clip=0.05, shape=750, size=100,
     if save_gp_to is not None:
         gpsave(gp, f'{save_gp_to}')
 # the following modifies GP:
-    data, err, x1x2 = gppredict(gp, shape=shape, size=size, normalize=True)
+    data, err, x1x2 = gppredict(gp, shape=shape, size=size, normalize=normalize)
     data = np.float32(data)
 
+    if spec_index_corr:
+        data = data * 1.02 # assuming apectral index = -0.7 and NVSS freq = 1.400
+
     header = fits.getheader(glob.glob(f'/kutkin/beams/{beam}/*.hdr')[0])
-    factor = 2.0 * shape / size # increaze of CDELT
+    factor = (2.0 * shape - 1) / size # increaze of CDELT
 
     header.update(NAXIS1=size, NAXIS2=size, CRPIX1=size/2, CRPIX2=size/2,
                   CDELT1=header['CDELT1']*factor, CDELT2=header['CDELT2']*factor)
@@ -242,6 +266,8 @@ def make_pbeam(tid, beam, maxdt=7, nnear=None, clip=0.05, shape=750, size=100,
     if out is None:
         out = os.path.join(f'{tid}_{beam}_pb.fits')
     hdu.writeto(out, overwrite=True)
+
+    # fits_squeeze(out)
     return gp, data, err, out
 
 
@@ -328,11 +354,25 @@ def mosaic_pbeams():
                   header=hdr, overwrite=True)
 
 
-def pball(beam):
+def make_averaged_gpbeam(beam):
+    df = pd.read_csv(f'/kutkin/beams/{beam}/APERTIF_NVSS_relation.dat',
+                     sep='\s+',
+                     names=['tid', 'beam', 'x', 'y', 'z'])
+    df['cluster'] = KMeans(1000).fit_predict(np.array([df.x, df.y]).T)
+    newdf = df.groupby('cluster').mean()
+    gp = gpr(newdf.x, newdf.y, newdf.z)
 
-    gp, data, err, out = make_pbeam(191010000, beam, maxdt=365,
-                                    out=f'/kutkin/beams/{beam}/pb_{beam}_gpall.fits',
-                                    save_gp_to=f'/kutkin/beams/{beam}/gpall.pkl')
+    gpfile = f'/kutkin/beams/{beam}/{beam}_gpavg_kmeans1000.pkl'
+    gpsave(gp, gpfile)
+
+    gp, data, err, out = make_pbeam(200101000, beam, maxdt=365, gpfile=gpfile,
+                                    size=200, shape=750,
+                                    normalize=False,
+                                    out=f'/kutkin/beams/{beam}/{beam}_gp_avg_nonorm.fits')
+    gp, data, err, out = make_pbeam(200101000, beam, maxdt=365, gpfile=gpfile,
+                                    normalize=True,
+                                    size=200, shape=750,
+                                    out=f'/kutkin/beams/{beam}/{beam}_gp_avg_norm.fits')
     # np.save(f'/kutkin/beams/{beam}/data_gpall.npy', data)
     # np.save(f'/kutkin/beams/{beam}/err_gpall.npy', err)
 
@@ -347,26 +387,31 @@ if __name__ == "__main__":
 
 
 #%% bulk process all the data to get the A/N relation
-    # from multiprocessing import Pool
     # beams = [str(_).zfill(2) for _ in range(40)]
+
     # p = Pool(40)
-    # p.map(process, beams)
+    # p.map(prepare_aptf_nvss_relation, beams)
 # use "for" if bdsf involved...
     # for b in beams[::2]:
     # for b in beams[::2][::-1]:
     # for b in beams[1::2]:
     # for b in beams[1::2][::-1]:
-        # process(b)
+        # prepare_aptf_nvss_relation(b)
 #%% compare to drift scans -- the latter seem to be WRONG, e.g. beam 01 (see files in /kutkin/test/compare_gpbeams_with_driftscans)
     # tid = 191120000 # fake TID corresponding to drift scans date
     # beam = 1 # beam 1 differs most dramatically -- 90 degrees rotation?
     # out, data, err, gp = make_pbeam(tid, beam, maxdt=14)
 
 #%% GPall (make pbeams for all beam images):
-    # from multiprocessing import Pool
     # beams = [str(_).zfill(2) for _ in range(40)]
+    # import time
+    # import psutil
+    # while psutil.pid_exists(12055):
+    #     logging.info('Waiting for 10 minutes')
+    #     time.sleep(600)
+
     # p = Pool(len(beams))
-    # p.map(pball, beams)
+    # p.map(make_averaged_gpbeam, beams)
 
 #%% mosaic plot -- works...
     # mosaic_pbeams()
@@ -375,6 +420,15 @@ if __name__ == "__main__":
 #    see figures script in acat
         # print(gp)
 
+#%% make full-size CBs for Betsey: does not work...
+    # beams = [str(_).zfill(2) for _ in range(40)]
+    # for beam in beams:
+    #     print(beam)
+    #     # gp = gpload(f'/kutkin/beams.bak/{beam}/gpall.pkl')
+    #     out = f'/kutkin/tmp/cbeams_fullsize/cb_{beam}.fits'
+    #     make_pbeam(0, beam, shape=1537, size=3073,
+    #                gpfile=f'/kutkin/beams.bak/{beam}/gpall.pkl',
+    #                out=out)
 
 
 
